7d0449fa638ac0f72517034bd4dc8cda7e94ea42
kavin <kavindra.senanayake@gmail.com>
2023-03-08 22:57:04 +0530

    modified:   index.php

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
         <form class="" action="send.php" method="post">
             <div class="info-wrap">
             <input type="text" name="info" value="" placeholder="Test Info"><br>
+            <input type="text" name="subject" value="" placeholder="Test Subject"><br>
             <input type="text" name="name" value="" placeholder="Enter your name"><br>
             <input type="text" name="email" value="" placeholder="Enter your email">
             <button type="submit" name="button">Send</button>
